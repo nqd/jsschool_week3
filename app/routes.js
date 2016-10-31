68f88db9655a7a61dc47281a4e5a89ee8f26efd6
nqdinh <dinh.nguyen@veriksystems.com>
2016-10-31 09:12:43 +0700

    like/unlike

--- app/routes.js
+++ app/routes.js
@@ -18,12 +18,12 @@ module.exports = (app) => {
     client.get('statuses/home_timeline', { count: 20 }, function(error, tweets, response) {
       let posts = tweets.map(obj => {
         return {
-          id: obj.id,
+          id: obj.id_str,
           image: obj.user.profile_image_url,
           text: obj.text,
           name: obj.description,
           username: obj.username,
-          liked: true,
+          liked: obj.favorited,
           network: {
             icon: 'twitter',
             name: 'Twitter',
@@ -32,6 +32,34 @@ module.exports = (app) => {
         }
       })
       res.render('timeline.ejs', {message: req.flash('error'), posts: posts})
+    })
+  })
+
+  app.post('/like/:id', isLoggedIn, (req, res) => {
+    let id = req.params.id
+    let client = new Twitter({
+      consumer_key: process.env.TWITTER_CONSUMER_KEY,
+      consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+      access_token_key: req.user.twitterId.token,
+      access_token_secret: req.user.twitterId.tokenSecret
+    });
+    client.post('favorites/create', { id: id }, function(error, tweets, response) {
+      console.log(`error ${error}`)
+      res.redirect('/timeline')
+    })
+  })
+
+  app.post('/unlike/:id', isLoggedIn, (req, res) => {
+    let id = req.params.id
+    let client = new Twitter({
+      consumer_key: process.env.TWITTER_CONSUMER_KEY,
+      consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+      access_token_key: req.user.twitterId.token,
+      access_token_secret: req.user.twitterId.tokenSecret
+    });
+    client.post('favorites/destroy', { id: id }, function(error, tweets, response) {
+      console.log(`error ${error}`)
+      res.redirect('/timeline')
     })
   })
 }
